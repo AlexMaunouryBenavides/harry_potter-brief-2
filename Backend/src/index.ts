02b0e32843a7b960aa6b73c1e2a5db1c7861b75d
maxime <maximelarquetoux@gmail.com>
2026-01-13 18:27:46 +0100

    chore(backend): initialize backend with dependencies, tsconfig for ESM, and Express server setup

---
+++ Backend/src/index.ts
@@ -0,0 +1,16 @@
+import express from "express";
+
+const app = express();
+
+const PORT = "4000";
+
+app.get("/", (req, res) => {
+res.status(200).send("Hello World");
+});
+
+app.listen(PORT, () => {
+console.log("Server running at PORT: ", PORT);
+}).on("error", (error) => {
+
+throw new Error(error.message);
+});
