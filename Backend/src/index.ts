481cedb2f664dce1d373c5d422737a969ee768e0
maxime <maximelarquetoux@gmail.com>
2026-01-16 11:03:59 +0100

    Added cors for link the back to the front

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -1,7 +1,10 @@
 import express from "express";
 import routes from "./routes/routes.js";
+import cors from 'cors';
 
 const app = express();
+
+app.use(cors());
 
 app.use(express.json());
 
@@ -16,6 +19,6 @@ res.status(200).send("Hello World");
 app.listen(PORT, () => {
 console.log("Server running at PORT: ", PORT);
 }).on("error", (error) => {
-    
+
 throw new Error(error.message);
 });
