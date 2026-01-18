d6bce3df3df9fdc19e7d12439a2ba94fd504609a
Alexandre Maunoury <alexandremaunoury@yahoo.fr>
2026-01-18 12:13:28 +0100

    connexion page character, added necesary functions

--- Frontend/dist/main.js
+++ Frontend/dist/main.js
@@ -1,7 +1,14 @@
 import characterFilter from "./utils/charactersFilter.js";
 import displayCards from "./utils/displayCards.js";
+import displayCharacter from "./utils/displayCharacter.js";
 import fetchData from "./utils/fetchData.js";
+import getCharacterId from "./utils/getCharacterId.js";
 async function init() {
+    const id = getCharacterId();
+    if (id) {
+        await displayCharacter(id);
+        return;
+    }
     const characters = await fetchData("http://localhost:4000/api/characters/");
     displayCards(characters);
     const input = document.querySelector("#filterInput");
--- Frontend/src/main.ts
+++ Frontend/src/main.ts
@@ -1,8 +1,15 @@
 import characterFilter from "./utils/charactersFilter.js";
 import displayCards from "./utils/displayCards.js";
+import displayCharacter from "./utils/displayCharacter.js";
 import fetchData from "./utils/fetchData.js";
+import getCharacterId from "./utils/getCharacterId.js";
 
 async function init() {
+  const id = getCharacterId();
+  if (id) {
+    await displayCharacter(id);
+    return;
+  }
   const characters = await fetchData("http://localhost:4000/api/characters/");
   displayCards(characters);
 
@@ -12,6 +19,7 @@ async function init() {
     const value = target.value.toLowerCase();
 
     const filtered = characterFilter(characters, value);
+
     displayCards(filtered);
   });
 }
